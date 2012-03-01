0e6ed85018dd1b6d86daba8c9a9c8f887ed85665
Yves Van Broekhoven <yvesvanbroekhoven@gmail.com>
2012-03-01 10:42:17 +0100

    Added chosen example page, added jquery ui sortable

--- jquery-chosen-sortable.js
+++ jquery-chosen-sortable.js
@@ -1,8 +1,19 @@
 (function($){
   
   $.fn.chosenSortable = function(){
+    $this = this.filter('.chzn-sortable');
     
+    $this.each(function(){
+      $select = $(this);
+      $chosen = $select.siblings('.chzn-container');
+
+      $chosen.find('.chzn-choices').sortable({
+        'items': 'li:not(.search-field)'
+      });
+      
+      
+    })
     
-  }
+  };
   
 }(jQuery));
